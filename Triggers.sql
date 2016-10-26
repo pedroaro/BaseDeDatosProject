80de8b2466e2eb37ae092e4893eb7d92d848a3ab
Pedro Rodriguez <pedroaro123@gmail.com>
2016-10-25 23:05:41 -0400

    Agregando los Triggers

---
+++ Triggers.sql
@@ -0,0 +1,17 @@
+--Triggers
+
+CREATE OR REPLACE TRIGGER only_sixten_trainers
+  BEFORE UPDATE OR INSERT ON Entrenador
+  FOR EACH ROW
+DECLARE
+  trainer_count  INTEGER;      --# of depts for this employee
+  max_trainer   INTEGER := 16; --max number of depts per trainer.
+BEGIN
+ 	SELECT COUNT(*) INTO trainer_count
+   	FROM Entrenador
+  	WHERE id_pokedex != NULL;
+    IF trainer_count >= max_trainer THEN
+    RAISE_APPLICATION_ERROR (-20000,'Trainers are limited to a max of 16');
+	END IF;
+END;
+/
