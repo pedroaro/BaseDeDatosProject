16b889a352b9bb6bc73d73284c8de243caf814aa
JosMMV <jsmiguel15@hotmail.com>
2016-10-25 23:18:34 -0400

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -10,8 +10,9 @@ BEGIN
  	SELECT COUNT(*) INTO trainer_count
    	FROM Entrenador
   	WHERE id_pokedex != NULL;
-    IF trainer_count >= max_trainer THEN
-    RAISE_APPLICATION_ERROR (-20000,'Trainers are limited to a max of 16');
+
+	IF trainer_count >= max_trainer THEN
+    		RAISE_APPLICATION_ERROR (-20000,'Trainers are limited to a max of 16');
 	END IF;
 END;
 /
