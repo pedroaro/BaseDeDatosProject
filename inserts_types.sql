6b857e5a89650cd25ccb657a10ad60a552603ecb
JosMMV <jsmiguel15@hotmail.com>
2016-10-25 22:08:23 -0400

    Add files via upload

---
+++ inserts_types.sql
@@ -0,0 +1,18 @@
+--Insertar a la tabla TIPO
+INSERT ALL
+INTO TIPO VALUES (1, 'normal')
+INTO TIPO VALUES (2, 'luchador')
+INTO TIPO VALUES (3, 'volador')
+INTO TIPO VALUES (4, 'veneno')
+INTO TIPO VALUES (5, 'tierra')
+INTO TIPO VALUES (6, 'roca')
+INTO TIPO VALUES (7, 'insecto')
+INTO TIPO VALUES (8, 'fantasma')
+INTO TIPO VALUES (9, 'fuego')
+INTO TIPO VALUES (10, 'agua')
+INTO TIPO VALUES (11, 'hierba')
+INTO TIPO VALUES (12, 'eléctrico')
+INTO TIPO VALUES (13, 'psíquico')
+INTO TIPO VALUES (14, 'hielo')
+INTO TIPO VALUES (15, 'dragón')
+SELECT 1 FROM DUAL;
