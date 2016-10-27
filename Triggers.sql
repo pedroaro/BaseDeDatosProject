5ed2ee17b7e8af30abda4c72928aaa6d45abe391
JosMMV <jsmiguel15@hotmail.com>
2016-10-26 20:05:41 -0400

    Trigger de la parte D de las inserciones

--- Triggers.sql
+++ Triggers.sql
@@ -1,5 +1,5 @@
 --Triggers
-
+--Parte A:
 CREATE OR REPLACE TRIGGER only_sixten_trainers
   BEFORE UPDATE OR INSERT ON Entrenador
   FOR EACH ROW
@@ -13,6 +13,23 @@ BEGIN
 
 	IF trainer_count >= max_trainer THEN
     		RAISE_APPLICATION_ERROR (-20000,'Trainers are limited to a max of 16');
+	END IF;
+END;
+/
+
+--Parte D:
+CREATE OR REPLACE TRIGGER unique_pokemon
+	BEFORE INSERT ON Equipo_Entrenador
+	FOR EACH ROW
+DECLARE
+	pokemons_of_team NUMBER;
+BEGIN
+	SELECT COUNT(id_pokemon) INTO pokemons_of_team
+	FROM Equipo_Entrenador
+	WHERE :NEW.id_pokemon = :OLD.id_pokemon;
+
+	IF (pokemons_of_team > 0) THEN
+		RAISE_APPLICATION_ERROR(-20601,'The team of the trainer can not have repeated pokemons');
 	END IF;
 END;
 /
