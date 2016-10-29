59d64c03d18734fa36428991a42fd5940c3e2d52
Pedro Rodriguez <pedroaro123@gmail.com>
2016-10-29 13:46:55 -0400

    ataques contra roca, cadena evolutiva

--- Querys.sql
+++ Querys.sql
@@ -16,3 +16,33 @@ WHERE id IN
 				WHERE nombre = 'agua'))
 ORDER BY ataque DESC;
 
+--Cadena Evolutiva
+SELECT *
+FROM Pokemon
+WHERE metodo_evolucion = 'intercambio' OR id IN
+	(	SELECT evoluciona_de
+		FROM Pokemon
+		WHERE metodo_evolucion = 'intercambio' OR id IN 
+		(	SELECT evoluciona_de
+			FROM Pokemon
+			WHERE metodo_evolucion = 'intercambio'
+		));
+
+--10 ataques mas fuertes contra roca
+SELECT B.nombre,B.efectividad, B.poder
+FROM (
+	SELECT A.nombre, T2.efectividad, CASE 	WHEN A.poder IS NULL THEN 0
+											ELSE A.poder END PODER
+	FROM Ataque A,
+		(	SELECT t.id, t1.efectividad
+			FROM Tipo t,(	SELECT id_tipo1, efectividad
+							FROM Tipo_Efectivo_Tipo
+							WHERE id_tipo2 IN 
+								(	SELECT id
+									FROM Tipo
+									WHERE nombre = 'roca'
+									)) t1
+			WHERE t.id = t1.id_tipo1 AND efectividad IN (SELECT MAX(efectividad) FROM Tipo_Efectivo_Tipo )) T2
+	WHERE T2.id = A.id_tipo
+	ORDER BY PODER DESC) B
+WHERE ROWNUM<= 10;
