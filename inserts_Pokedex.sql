91be9e53bbe164d83d9b3352a6585e41a3b06be0
Pedro Rodriguez <pedroaro123@gmail.com>
2016-10-29 14:18:57 -0400

    insert pokedex

---
+++ inserts_Pokedex.sql
@@ -0,0 +1,19 @@
+--Insertar a la tabla pokedex
+INSERT ALL
+INTO Pokedex VALUES (1, 50, 32)
+INTO Pokedex VALUES (2, 30, 30)
+INTO Pokedex VALUES (3, 60, 12)
+INTO Pokedex VALUES (4, 40, 53)
+INTO Pokedex VALUES (5, 23, 65)
+INTO Pokedex VALUES (6, 41, 34)
+INTO Pokedex VALUES (7, 45, 33)
+INTO Pokedex VALUES (8, 86, 43)
+INTO Pokedex VALUES (9, 11, 12)
+INTO Pokedex VALUES (10, 34, 54)
+INTO Pokedex VALUES (11, 75, 64)
+INTO Pokedex VALUES (12, 90, 23)
+INTO Pokedex VALUES (13, 20, 23)
+INTO Pokedex VALUES (14, 54, 12)
+INTO Pokedex VALUES (15, 87, 14)
+INTO Pokedex VALUES (16, 97, 57)
+SELECT 1 FROM DUAL;
