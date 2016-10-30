e99e5644f04589889b434133edae7ce58b080acb
JosMMV <jsmiguel15@hotmail.com>
2016-10-30 16:13:06 -0400

    Corrección de trigger 'refresh_championship'

--- Triggers.sql
+++ Triggers.sql
@@ -88,32 +88,23 @@ CREATE OR REPLACE TRIGGER refresh_championship
 	AFTER INSERT ON Batalla
 	FOR EACH ROW
 DECLARE
-	contador NUMBER;
-	today DATE;
+	contador NUMBER(3);
 BEGIN
 	SELECT COUNT(*) INTO contador
 	FROM Resumen_Torneo;
-
-	SELECT sysdate INTO today
-	FROM dual;
-
-	IF(:NEW.entrenador_ganador != NULL) THEN
+	IF( :NEW.entrenador_ganador IS NOT NULL ) THEN
 		IF (contador < 8) THEN
 			INSERT INTO Resumen_Torneo
-			VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Octavos', today, :NEW.entrenador_ganador);
-		ELSE
-			IF (contador < 12) THEN
+			VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Octavos', TO_DATE('2016-11-11','YYYY-MM-DD'), :NEW.entrenador_ganador);
+		ELSIF (contador < 12) THEN
 				INSERT INTO Resumen_Torneo
-				VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Cuartos', today, :NEW.entrenador_ganador);
-			ELSE
-				IF (contador < 14) THEN
+				VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Cuartos', TO_DATE('2016-11-12','YYYY-MM-DD'), :NEW.entrenador_ganador);
+			ELSIF (contador < 14) THEN
 					INSERT INTO Resumen_Torneo
-					VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Semifinal', today, :NEW.entrenador_ganador);
-				ELSIF (contador = 15) THEN
-					INSERT INTO Resumen_Torneo
-					VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Final', today, :NEW.entrenador_ganador);
-				END IF;
-			END IF;
+					VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Semifinal', TO_DATE('2016-11-13','YYYY-MM-DD'), :NEW.entrenador_ganador);
+				ELSIF (contador = 14) THEN
+						INSERT INTO Resumen_Torneo
+						VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Final', TO_DATE('2016-11-15','YYYY-MM-DD'), :NEW.entrenador_ganador);
 		END IF;
 	END IF;
 END;
