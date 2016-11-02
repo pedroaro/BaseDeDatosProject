2682db4553c74966388df14bb82386dd75f6d13c
Pedro Rodriguez <pedroaro123@gmail.com>
2016-11-02 15:31:24 -0400

    Agregar id de batalla en trigger

--- Triggers.sql
+++ Triggers.sql
@@ -9,16 +9,16 @@ BEGIN
 	IF( :NEW.entrenador_ganador IS NOT NULL ) THEN
 		IF (contador < 8) THEN
 			INSERT INTO Resumen_Torneo
-			VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Octavos', TO_DATE('2016-11-11','YYYY-MM-DD'), :NEW.entrenador_ganador);
+			VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Octavos', TO_DATE('2016-11-11','YYYY-MM-DD'),:NEW.id_batalla, :NEW.entrenador_ganador);
 		ELSIF (contador < 12) THEN
 				INSERT INTO Resumen_Torneo
-				VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Cuartos', TO_DATE('2016-11-12','YYYY-MM-DD'), :NEW.entrenador_ganador);
+				VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Cuartos', TO_DATE('2016-11-12','YYYY-MM-DD'),:NEW.id_batalla, :NEW.entrenador_ganador);
 			ELSIF (contador < 14) THEN
 					INSERT INTO Resumen_Torneo
-					VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Semifinal', TO_DATE('2016-11-13','YYYY-MM-DD'), :NEW.entrenador_ganador);
+					VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Semifinal', TO_DATE('2016-11-13','YYYY-MM-DD'),:NEW.id_batalla, :NEW.entrenador_ganador);
 				ELSIF (contador = 14) THEN
 						INSERT INTO Resumen_Torneo
-						VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Final', TO_DATE('2016-11-15','YYYY-MM-DD'), :NEW.entrenador_ganador);
+						VALUES (:NEW.id_entrenador1, :NEW.id_entrenador2, 'Final', TO_DATE('2016-11-15','YYYY-MM-DD'),:NEW.id_batalla, :NEW.entrenador_ganador);
 		END IF;
 	END IF;
 END;
