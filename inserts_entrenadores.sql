99d744aab0405625b7f0e08cbf613159cb141ab2
JosMMV <jsmiguel15@hotmail.com>
2016-11-02 23:46:41 -0400

    Eliminación de acentos

--- inserts_entrenadores.sql
+++ inserts_entrenadores.sql
@@ -3,8 +3,8 @@ INTO Entrenador VALUES (1, 'Pepe', 'Valencia', 49, to_date('18-08-2012', 'DD-MM-
 INTO Entrenador VALUES (2, 'Pedro', 'Caracas', 62, to_date('18-09-2015', 'DD-MM-YYYY'), 2) 
 INTO Entrenador VALUES (3, 'Alexis', 'Vancouver', 82, to_date('18-05-2014', 'DD-MM-YYYY'), 3)
 INTO Entrenador VALUES (4, 'Maria', 'Maracay', 52, to_date('16-08-2016', 'DD-MM-YYYY'), 4)
-INTO Entrenador VALUES (5, 'José', 'Valencia', 64, to_date('17-03-2012', 'DD-MM-YYYY'), 5)--
-INTO Entrenador VALUES (6, 'Ángeles', 'Caracas', 84, to_date('28-01-2014', 'DD-MM-YYYY'), 6)
+INTO Entrenador VALUES (5, 'Jose', 'Valencia', 64, to_date('17-03-2012', 'DD-MM-YYYY'), 5)--
+INTO Entrenador VALUES (6, 'Angeles', 'Caracas', 84, to_date('28-01-2014', 'DD-MM-YYYY'), 6)
 INTO Entrenador VALUES (7, 'Juan', 'Maracaibo', 48, to_date('12-02-2016', 'DD-MM-YYYY'), 7)
 INTO Entrenador VALUES (8, 'Miguel', 'Caracas', 63, to_date('12-09-2010', 'DD-MM-YYYY'), 8)
 INTO Entrenador VALUES (9, 'Andres', 'Valencia', 83, to_date('05-10-2010', 'DD-MM-YYYY'), 9)
